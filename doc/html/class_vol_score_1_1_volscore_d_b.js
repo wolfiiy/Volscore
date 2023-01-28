d4adcaa0e8974f9238d1b2d9292d94506da39f9b
XCarrel <Xavier.Carrel@cpnv.ch>
2023-01-28 12:58:15 +0100

    Documenter GetTeam et GetCaptain

--- doc/html/class_vol_score_1_1_volscore_d_b.js
+++ doc/html/class_vol_score_1_1_volscore_d_b.js
@@ -4,10 +4,12 @@ var class_vol_score_1_1_volscore_d_b =
     [ "AddSet", "class_vol_score_1_1_volscore_d_b.html#a507f52d8d3a7a1e2e3d992ae7327a311", null ],
     [ "CreateGame", "class_vol_score_1_1_volscore_d_b.html#a70ce3250fb80dd6157bfed61fc4432fc", null ],
     [ "DefinePlayersPositions", "class_vol_score_1_1_volscore_d_b.html#a326126390990ebe454198a3260e91313", null ],
+    [ "GetCaptain", "class_vol_score_1_1_volscore_d_b.html#a5d21cb536e95f925d62a9185de0d0b45", null ],
     [ "GetGame", "class_vol_score_1_1_volscore_d_b.html#a9ce9512c25b225e077ff59aaca3ec3a5", null ],
     [ "GetGames", "class_vol_score_1_1_volscore_d_b.html#a61090f39370f4086fa123af27fbc2a39", null ],
     [ "GetPlayers", "class_vol_score_1_1_volscore_d_b.html#aac09fea94e4db73f311516171489c20a", null ],
     [ "GetSet", "class_vol_score_1_1_volscore_d_b.html#a8051996cc0f43abc3ca802a49d4ce16a", null ],
+    [ "GetTeam", "class_vol_score_1_1_volscore_d_b.html#a4d65728a7bd06a23b4f0bf9024d8c7db", null ],
     [ "GetTeams", "class_vol_score_1_1_volscore_d_b.html#a5c79d00dc7effcc1dd5d143e1ade7c88", null ],
     [ "NumberOfSets", "class_vol_score_1_1_volscore_d_b.html#a0b9f6038efcea344de7d9ebd3f7c1ed6", null ],
     [ "SelectAllUsers", "class_vol_score_1_1_volscore_d_b.html#ac393896046b7ec0fb0b5ebfe77c68d26", null ]
--- doc/html/interface_vol_score_1_1_i_volscore_d_b.js
+++ doc/html/interface_vol_score_1_1_i_volscore_d_b.js
@@ -7,10 +7,12 @@ var interface_vol_score_1_1_i_volscore_d_b =
     [ "AddSet", "interface_vol_score_1_1_i_volscore_d_b.html#a5e6cf534bc1380436bba0db4b1b21fba", null ],
     [ "CreateGame", "interface_vol_score_1_1_i_volscore_d_b.html#ac9d57f6ad7e7a38729540831c079e20b", null ],
     [ "DefinePlayersPositions", "interface_vol_score_1_1_i_volscore_d_b.html#a8cf9ba77cc65f19f595474288e0c2fe2", null ],
+    [ "GetCaptain", "interface_vol_score_1_1_i_volscore_d_b.html#a98ac7d2a67c0e68b11339ea87d899036", null ],
     [ "GetGame", "interface_vol_score_1_1_i_volscore_d_b.html#a362a16e817f4a491c3439019ce121954", null ],
     [ "GetGames", "interface_vol_score_1_1_i_volscore_d_b.html#acf69d26ed24ed96b35fb40a0a90ba558", null ],
     [ "GetPlayers", "interface_vol_score_1_1_i_volscore_d_b.html#a46073070d240be5a3ad1030aa28bc540", null ],
     [ "GetSet", "interface_vol_score_1_1_i_volscore_d_b.html#a83353ca6efcf232dd9c53a536792cfd8", null ],
+    [ "GetTeam", "interface_vol_score_1_1_i_volscore_d_b.html#a0423185b67a99aa104577a74e37d6084", null ],
     [ "GetTeams", "interface_vol_score_1_1_i_volscore_d_b.html#a380e2ad465ceb3924c4627e9fe89cce4", null ],
     [ "NumberOfSets", "interface_vol_score_1_1_i_volscore_d_b.html#ad6d572ac91d4630f284f5bbf12cf66b5", null ]
 ];
