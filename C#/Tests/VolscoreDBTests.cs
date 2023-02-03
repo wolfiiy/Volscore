23c9bd289397aef3c7314f1fd85f9c38fc6effc7
Xavier Carrel <px05iql@eduvaud.ch>
2023-02-03 10:41:46 +0100

    C# La db est réinitialisée dans une méthode ClassInit

--- C#/Tests/VolscoreDBTests.cs
+++ C#/Tests/VolscoreDBTests.cs
@@ -1,4 +1,5 @@
 using MySql.Data.MySqlClient;
+using System.Diagnostics;
 using VolScore;
 
 namespace VolScore
@@ -9,13 +10,20 @@ namespace VolScore
     [TestClass]
     public class VolscoreDBTests
     {
-        [TestMethod]
-        public void GetTeamsTest()
+        static VolscoreDB vdb;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
         {
-            VolscoreDB vdb = new VolscoreDB();
+            vdb = new VolscoreDB();
             string script = File.ReadAllText(@"..\..\..\..\..\database\volscore.sql");
             MySqlCommand cmd = new MySqlCommand(script,vdb.Connection);
             cmd.ExecuteNonQuery();
+        }
+
+        [TestMethod]
+        public void GetTeamsTest()
+        {
             List<IVolscoreDB.Team> teams = vdb.GetTeams();
             Assert.AreEqual(6, teams.Count);
         }
@@ -23,7 +31,6 @@ namespace VolScore
         [TestMethod]
         public void GetTeamTest()
         {
-            VolscoreDB vdb = new VolscoreDB();
             IVolscoreDB.Team team = vdb.GetTeam(3);
             Assert.AreEqual("Froideville", team.Name);
         }
@@ -31,7 +38,6 @@ namespace VolScore
         [TestMethod]
         public void GetGameTest()
         {
-            VolscoreDB vdb = new VolscoreDB();
             IVolscoreDB.Game game = vdb.GetGame(1);
             // check some fields
             Assert.AreEqual("Championnat", game.Type);
@@ -42,7 +48,6 @@ namespace VolScore
         [TestMethod]
         public void GetCaptainTest()
         {
-            VolscoreDB vdb = new VolscoreDB();
             IVolscoreDB.Member cap = vdb.GetCaptain(vdb.GetTeam(3)); // Froideville
             Assert.AreEqual("Stewart", cap.LastName);
         }
@@ -50,7 +55,6 @@ namespace VolScore
         [TestMethod]
         public void GetLiberoTest()
         {
-            VolscoreDB vdb = new VolscoreDB();
             IVolscoreDB.Member lib = vdb.GetLibero(vdb.GetTeam(2)); // Froideville
             Assert.AreEqual("Eaton", lib.LastName);
         }
@@ -58,12 +62,11 @@ namespace VolScore
         [TestMethod]
         public void GetGamesTests()
         {
-            VolscoreDB vdb = new VolscoreDB();
             List<IVolscoreDB.Game> games = vdb.GetGames();
             Assert.AreEqual(3, games.Count);
             Assert.AreEqual("U17", games[0].League);
-            Assert.AreEqual("Yverdon", games[1].Place);
-            Assert.AreEqual("LUC", games[2].ReceivingTeamName);
+            Assert.AreEqual("Dorigny", games[1].Place);
+            Assert.AreEqual("Yverdon", games[2].ReceivingTeamName);
         }
     }
 }
