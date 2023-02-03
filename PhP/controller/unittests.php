6338c8c13a0024000148617fe8ff68563fb1d0a5
Xavier Carrel <px05iql@eduvaud.ch>
2023-02-03 16:30:34 +0100

    PhP Ajouter des matches futurs à l'initialisation des tests

--- PhP/controller/unittests.php
+++ PhP/controller/unittests.php
@@ -3,12 +3,34 @@
 // Reset database completely
 shell_exec("\"C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\mysql\"  -u root -proot < ..\\Database\\volscore.sql");
 
+// Add some games
+$today = date("Y-m-d");
+$rec = rand(1,6);
+$vis = rand(1,6);
 $query =
-    "INSERT INTO games (type,level,category,league,location,venue,moment,receiving_id,visiting_id) ".
-    "VALUES('Coupe', 'Régional-Vaud', 'F', 'F2', 'Froideville', 'Complexe sportif', '2023-03-03 20:00', 1, 2);";
-
+"INSERT INTO games (type,level,category,league,location,venue,moment,receiving_id,visiting_id) ".
+"VALUES('Coupe', 'Régional-Vaud', 'F', 'F2', 'Froideville', 'Complexe sportif', '$today 20:00', $rec, $vis);";
 $newgame = VolscoreDB::executeInsertQuery($query);
-dd($newgame);
+
+$tomorrow = date('Y-m-d', strtotime($today. ' + 1 days'));
+$rec = rand(1,6);
+$vis = rand(1,6);
+$query =
+"INSERT INTO games (type,level,category,league,location,venue,moment,receiving_id,visiting_id) ".
+"VALUES('Coupe', 'Régional-Vaud', 'F', 'F2', 'Lausanne', 'Vennes', '$tomorrow 21:00', $rec, $vis);";
+$newgame = VolscoreDB::executeInsertQuery($query);
+
+$aftertomorrow = date('Y-m-d', strtotime($today. ' + 2 days'));
+$rec = rand(1,6);
+$vis = rand(1,6);
+$query =
+"INSERT INTO games (type,level,category,league,location,venue,moment,receiving_id,visiting_id) ".
+"VALUES('Coupe', 'Régional-Vaud', 'F', 'F2', 'Lutry', 'Les Pales', '$aftertomorrow 20:45', $rec, $vis);";
+$newgame = VolscoreDB::executeInsertQuery($query);
+
+
+
+// Start those tests now 
 
 echo "Test getTeam(number) -> ";
 $team = VolscoreDB::getTeam(3);
