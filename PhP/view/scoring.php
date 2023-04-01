de6f8f2df03ed2f928b72d42e2e51b585d383f51
XCarrel <Xavier.Carrel@cpnv.ch>
2023-04-01 14:27:20 +0200

    Changer les équipes de côté à chaque set

--- PhP/view/scoring.php
+++ PhP/view/scoring.php
@@ -4,40 +4,43 @@ $title = 'Score du match '.$game->number;
 ob_start();
 ?>
 
-<table>
-    <tr><td colspan=2><h1>Set <?= $set->number ?></h1></td></tr>
-    <tr><td class="teamname"><?= $game->receivingTeamName ?></td><td class="teamname"><?= $game->visitingTeamName ?></td></tr>
-    <tr><td class="setscore"><?= $game->scoreReceiving ?></td><td class="setscore"><?= $game->scoreVisiting ?></td></tr>
-    <tr><td class="score"><?= $set->scoreReceiving ?></td><td class="score"><?= $set->scoreVisiting ?></td></tr>
-    <tr>
-        <td>
+<div class="row text-center"><h1>Set <?= $set->number ?></h1></div>
+<div class="d-flex flex-row justify-content-around">
+    <div class="d-flex flex-column order-<?= (($game->toss+$set->number) % 2 == 0) ? 1 : 2 ?>">
+        <div class="teamname"><?= $game->receivingTeamName ?></div>
+        <div class="setscore"><?= $game->scoreReceiving ?></div>
+        <div class="score"><?= $set->scoreReceiving ?></div>
+        <div>
             <?php foreach ($receivingPositions as $player) : ?>
                 <ul class="<?= ($player->id == $nextUp->id ? 'serving' : '') ?>"><?= $player->number ?> <?= $player->last_name ?></ul>
             <?php endforeach; ?>
-        </td>
-        <td>
-            <?php foreach ($visitingPositions as $player) : ?>
-                <ul class="<?= ($player->id == $nextUp->id ? 'serving' : '') ?>"><?= $player->number ?> <?= $player->last_name ?></ul>
-            <?php endforeach; ?>
-        </td>
-    </tr>
-    <tr>
-        <td class="actions">
+        </div>
+        <div class="actions">
             <form method="post" action="?action=scorePoint">
                 <input type="hidden" name="setid" value="<?= $set->id ?>" />
                 <input type="hidden" name="receiving" value="1" />
                 <input type="submit" value="Point" />
             </form>
-        </td>
-        <td class="actions">
+        </div>
+    </div>
+    <div class="d-flex flex-column order-<?= (($game->toss+$set->number) % 2 == 0) ? 2 : 1 ?>">
+        <div class="teamname"><?= $game->visitingTeamName ?></div>
+        <div class="setscore"><?= $game->scoreVisiting ?></div>
+        <div class="score"><?= $set->scoreVisiting ?></div>
+        <div>
+            <?php foreach ($visitingPositions as $player) : ?>
+                <ul class="<?= ($player->id == $nextUp->id ? 'serving' : '') ?>"><?= $player->number ?> <?= $player->last_name ?></ul>
+            <?php endforeach; ?>
+        </div>
+        <div class="actions">
             <form method="post" action="?action=scorePoint">
                 <input type="hidden" name="setid" value="<?= $set->id ?>" />
                 <input type="hidden" name="receiving" value="0" />
                 <input type="submit" value="Point" />
             </form>
-        </td>
-    </tr>
-</table>
+        </div>
+    </div>
+</div>
 
 <?php
 $content = ob_get_clean();
