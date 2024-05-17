798d3feaa7800d9a543664eb3afa37ee18b019d7
StreallyX <aproject37@gmail.com>
2024-05-17 14:41:57 +0200

    chore(credential-mail) : Changer l'exemple

--- PhP/lamp/www/model/credentials-mail-example.php
+++ PhP/lamp/www/model/credentials-mail-example.php
@@ -1,15 +1,9 @@
 <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+$mailHost= 'test.gmail.com';
+$mailSMTPAuth= 'test@gmail.com';
+$mailusername= 'test@gmail.com';
+$mailpassword = 'test test test test';
+$mailPort= 587;
 
-// Paramètres du serveur
-$mail->isSMTP();
-$mail->Host       = 'smtp.gmail.com';
-$mail->SMTPAuth   = true;
-$mail->Username   = 'test@gmail.com';
-$mail->Password   = 'qwer qwer qwer qwer';
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-$mail->Port       = 587;
 ?>
