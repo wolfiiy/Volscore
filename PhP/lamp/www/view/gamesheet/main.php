c30ec88dff0762c28765263378302ae2ccb7d159
StreallyX <aproject37@gmail.com>
2024-05-29 08:47:29 +0200

    UI(Affichage QRCode) : Changer l'emplacement des QRCode

--- PhP/lamp/www/view/gamesheet/main.php
+++ PhP/lamp/www/view/gamesheet/main.php
@@ -7,6 +7,12 @@ ob_start();
     <div id="gamedescription"><?php require_once 'view/gamesheet/description.php' ?></div> 
     <div id="gameresultdetails"><?php require_once 'view/gamesheet/resultdetails.php' ?></div> 
     <div id="sheetfooter"><?php require_once 'view/gamesheet/sheetfooter.php' ?></div> 
+    <div class="qrcode">
+    <h3>Profil du marqueur</h3>
+    <img src="/qrcode/qrcode1.png" alt="QR Code"/>
+    <h3>Profil de l'arbitre</h3>
+    <img src="/qrcode/qrcode2.png" alt="QR Code"/>
+</div>
 </div>
 <?php
 $content = ob_get_clean();
