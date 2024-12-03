c3a2aea838866849e645aedc3c81bcf7a74e475a
Santex0p <pn63pgk@eduvaud.ch>
2024-12-03 15:51:34 +0100

    refactor(css refaite): css refaite

--- PhP/lamp/www/view/home.php
+++ PhP/lamp/www/view/home.php
@@ -3,24 +3,19 @@ $title = 'Accueil';
 ob_start();
 ?>
 <div class="row">
-    <div class="col-6">
-        <a href="?action=teams">
-            <h1>Equipes</h1>
-        </a>
-    </div>
-    <div class="col-6">
-        <a href="?action=games">
-            <h1>Matches</h1>
-        </a>
-    </div>
-    <div id="info">
-        <a href="?action=info">
-            <button>Aide</button>
-        </a>
-    </div>
+<div class="col-6">
+    <h1>VolScores - Application</h1>
+    <p class="description">
+        VolScores est une application conçue pour faciliter l'organisation des matchs de volley-ball. 
+        Elle fournit des informations complètes sur les équipes, permettant aux utilisateurs de suivre les scores en temps réel. 
+        L'application enregistre également le temps de chaque match, tout en offrant toutes les fonctionnalités nécessaires pour la gestion des scores et la notation des matchs de volley-ball. 
+        Grâce à VolScores, les utilisateurs peuvent facilement suivre l'évolution d'un match, visualiser les résultats des équipes et obtenir des informations précieuses pour organiser des compétitions de volley-ball.
+    </p>
+</div>
 </div>
 <?php
 $content = ob_get_clean();
 require_once 'gabarit.php';
 ?>
+
 
